f11af4237aa27325440f8f264738ee72bd0cf20f
Nico314159 <nicolino.will@gmail.com>
2024-06-02 19:10:08 -0700

    Make disguised weapons look normal in 1st person

---
+++ TF2 Resource Pack - By Nicoder/assets/tf2/textures/disguise.py
@@ -0,0 +1,49 @@
+from itertools import product
+from os import getcwd, makedirs, mkdir
+from shutil import rmtree
+from PIL.Image import Image, open
+from pathlib import Path
+
+def make_empty_folder(path: Path):
+    if not path.exists():
+        return mkdir(path)
+
+    for content in path.iterdir():
+        if content.is_file():
+            content.unlink()
+        elif content.is_dir():
+            rmtree(content)
+        
+
+def make_altered_image(original: Image, name: str, alpha: int):
+    out_path = 'disguise' / (name / pic.relative_to(top_dir))
+    makedirs(out_path.parent, exist_ok=True)
+
+    out_img = original.copy()
+
+    width, height = original.size
+    for xy in product(range(width), range(height)):
+        *rgb, a = original.getpixel(xy)
+
+        if a == 0: continue
+
+        if a == 255:
+            out_img.putpixel(xy, (*rgb, alpha))
+    
+    out_img.save(out_path)
+            
+
+cwd = getcwd()
+items_path = Path(cwd, "item").relative_to(cwd)
+make_empty_folder(Path(cwd, 'disguise'))
+
+for pic in items_path.rglob('*.png'):
+    if pic.stem in {'logo', 'icon', 'transparent', 'white'}:
+        continue
+
+    with open(pic) as img:
+        top_dir = Path(*pic.parts[0:2])
+        make_altered_image(img, 'world', 254)
+
+        if top_dir.stem == "spy": 
+            make_altered_image(img, 'hand', 253)
